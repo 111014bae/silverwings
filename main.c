4d600e076d1b85082d176efdd286b9d06b430d4b
kim <>
2019-11-05 22:39:31 +0900

    delete trash line

--- main.c
+++ main.c
@@ -3,12 +3,11 @@
 
 int main(void)
 {
-<<<<<<< HEAD
-=======
+
 	printf("chief name is[이찬영]\n");
 	printMyname1();
 	printMyname2();
 	printMyName3(); //kim
 	printMyname4();
->>>>>>> a2847886797eb9c116c80a13376e5bed99b806f2
+
 }
